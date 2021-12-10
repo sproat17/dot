1c417ebbd0348dc0b6d2ee60f0865e44b7419865
Edward Sproat <82000483+sproat17@users.noreply.github.com>
2021-12-09 20:57:54 -0600

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(exwm-floating-border-color "#0b0f17")
+ '(fci-rule-color "#64727d")
+ '(highlight-tail-colors ((("#242e3a") . 0) (("#223040") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#070A0E" "#B16E75"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#070A0E" "#A3B09A"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#070A0E" "#64727d"))
+ '(objed-cursor-color "#c6797e")
+ '(package-selected-packages
+   '(go-autocomplete go-guru vterm cyberpunk-theme treemacs-magit modus-vivendi-theme evil-tutor))
+ '(pdf-view-midnight-colors (cons "#FAFFF6" "#172030"))
+ '(rustic-ansi-faces
+   ["#172030" "#c6797e" "#A3B09A" "#F7E3AF" "#6e94b9" "#b18bb1" "#88C0D0" "#FAFFF6"])
+ '(vc-annotate-background "#172030")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A3B09A")
+    (cons 40 "#bfc1a1")
+    (cons 60 "#dbd2a8")
+    (cons 80 "#F7E3AF")
+    (cons 100 "#f2d6a8")
+    (cons 120 "#eecaa1")
+    (cons 140 "#eabe9a")
+    (cons 160 "#d7ada1")
+    (cons 180 "#c39ba9")
+    (cons 200 "#b18bb1")
+    (cons 220 "#b884a0")
+    (cons 240 "#bf7f8f")
+    (cons 260 "#c6797e")
+    (cons 280 "#ad777d")
+    (cons 300 "#95757d")
+    (cons 320 "#7c737d")
+    (cons 340 "#64727d")
+    (cons 360 "#64727d")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ packages.el
@@ -0,0 +1,1 @@
+(package! modus-vivendi)
